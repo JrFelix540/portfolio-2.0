83a0f0a4798af9506db971549bb3f8788916a97d
Junior Felix <juniorfelix8825@outlook.com>
2021-03-31 22:06:01 +0300

    ✨ FEAT: Adds meta tags to head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React, { Fragment } from "react";
+import Head from "next/head";
 import Contact from "../components/Contact";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
@@ -6,6 +7,35 @@ import Projects from "../components/Projects";
 const Index: React.FC = () => {
     return (
         <Fragment>
+            <Head>
+                <title>Junior Felix | Portfolio</title>
+                <meta property="og:type" content="website" />
+                <meta
+                    property="og:title"
+                    content="Junior Felix | Portfolio website"
+                />
+                <meta
+                    property="og:description"
+                    content="A portfolio website created by Junior Felix"
+                />
+                <meta
+                    name="description"
+                    content="A portfolio website created by Junior Felix"
+                />
+                <link rel="icon" type="image/png" sizes="32x32" />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="./img/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="./img/favicon-16x16.png"
+                />
+            </Head>
             <Header />
             <Hero />
             <Projects />
